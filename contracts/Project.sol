c70a978c4e2b919a8f5d9f86b4ee61d7c275bcfe
MohitRustagi09 <randchikoo@gmail.com>
2025-10-25 16:47:00 +0530

    Update Project.sol
    Wrote a small contract showing the project name and description

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-pragma solidity >=0.8.2 <0.9.0;
+contract Identity {
+    string Project;
+    string Description;
 
-contract DAO-Creation-Toolkit {
+    constructor() {
+        Project = "DAO-Creation-Toolkit";
+        Description = unicode"The DAO Creation Toolkit is a blockchain-based framework that enables users to easily create, customize, and deploy Decentralized Autonomous Organizations (DAOs). It provides modular governance, treasury, and voting smart contracts for transparent, community-driven decision-making.";
+    }
+    
+    function showProject() public view returns (string memory) {
+        return Project;
+    }
+    
+    function showDescription() public view returns (string memory) {
+        return Description; 
+    }
 }
