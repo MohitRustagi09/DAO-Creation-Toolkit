b7b48e027bcffaa170a37ca5e7c0bca388e24cc7
MohitRustagi09 <randchikoo@gmail.com>
2025-10-26 20:10:00 +0530

    Update Project.sol
    added startdate

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -4,10 +4,12 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Identity {
     string Project;
     string Description;
+    int startdate;
 
     constructor() {
         Project = "DAO-Creation-Toolkit";
         Description = unicode"The DAO Creation Toolkit is a blockchain-based framework that enables users to easily create, customize, and deploy Decentralized Autonomous Organizations (DAOs). It provides modular governance, treasury, and voting smart contracts for transparent, community-driven decision-making.";
+        startdate = 23102006;
     }
     
     function showProject() public view returns (string memory) {
@@ -16,5 +18,8 @@ contract Identity {
     
     function showDescription() public view returns (string memory) {
         return Description; 
+    }
+    function showstartDate() public view returns (int memory) {
+        return startdate; 
     }
 }
